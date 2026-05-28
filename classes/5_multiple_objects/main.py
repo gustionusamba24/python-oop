200d3b3ab155f14fb3343d43c4841e6ae48eec3a
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-05-28 20:25:47 +0700

    CLASSES: finish the multiple object class

---
+++ classes/5_multiple_objects/main.py
@@ -0,0 +1,29 @@
+def main() -> None:
+    aragorn = Brawler("Aragorn", 4, 4)
+    gimli = Brawler("Gimli", 2, 7)
+    legolas = Brawler("Legolas", 7, 7)
+    frodo = Brawler("Frodo", 3, 2)
+
+    fight(aragorn, gimli)
+    fight(legolas, frodo)
+
+class Brawler:
+    def __init__(self, name, speed, strength):
+        self.name = name
+        self.speed = speed
+        self.strength = strength
+        self.power = speed * strength
+
+def fight(attacker, defender):
+    print(f"{attacker.name}: {attacker.power} power")
+    print(f"{defender.name}: {defender.power} power")
+
+    if attacker.power > defender.power:
+        print(f"{attacker.name} wins")
+    elif attacker.power < defender.power:
+        print(f"{defender.name} wins")
+    else:
+        print("It's a tie")
+    print("=========================================")
+
+main()
