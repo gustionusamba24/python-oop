a98183825dca3f66d66bfc05ee98566f8df14d49
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-05-29 19:39:43 +0700

    ENCAPSULATIONS: finish the encapsulation basic task

---
+++ encapsulations/1_encapsulation_basic/main.py
@@ -0,0 +1,19 @@
+class Wizard:
+    """
+    1. Set 2 private properties (be sure to include the private __ prefix):
+        1. stamina
+        2. intelligence
+    """
+
+    """
+    2. Set 3 public properties:
+        1. name: Use the value passed into the constructor
+        2. health: 100x the value of "stamina"
+        3. mana: 10x the value of "intelligence"
+    """
+    def __init__(self, name, stamina, intelligence):
+        self.name = name
+        self.__stamina = stamina
+        self.__intelligence = intelligence
+        self.health = stamina * 100
+        self.mana = intelligence * 10
