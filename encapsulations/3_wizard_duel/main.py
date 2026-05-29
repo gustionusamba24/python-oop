33fab955fad3f37ce8a9e43b0d63aa62d7318640
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-05-29 20:43:39 +0700

    ENCAPSULATIONS: finish the wizard duel part II task

---
+++ encapsulations/3_wizard_duel/main.py
@@ -0,0 +1,33 @@
+class Wizard:
+    def __init__(self, name, stamina, intelligence):
+        self.name = name
+        self.__stamina = stamina
+        self.__intelligence = intelligence
+        self.mana = self.__intelligence * 10
+        self.health = self.__stamina * 100
+
+
+    """
+    1. Complete the cast_fireball method:
+        1. If there isn't enough mana to cast a fireball (based on the fireball_cost argument), raise an Exception with the message ____ cannot cast fireball, where ____ is the wizard's name.
+        2. If the wizard has enough mana, reduce their mana by the fireball_cost and call get_fireballed on the target wizard with the given fireball_damage.
+    """
+    def cast_fireball(self, target, fireball_cost, fireball_damage):
+        if self.mana < fireball_cost:
+            raise Exception(f"{self.name} cannot cast fireball")
+        self.mana -= fireball_cost
+        target.get_fireballed(fireball_damage)
+
+    """
+    2. Complete the is_alive method. It should return True if the wizard's health is greater than 0 and False otherwise.
+    """
+    def is_alive(self):
+        return self.health > 0
+
+    def get_fireballed(self, fireball_damage):
+        fireball_damage -= self.__stamina
+        self.health -= fireball_damage
+
+    def drink_mana_potion(self, potion_mana):
+        potion_mana += self.__intelligence
+        self.mana += potion_mana
